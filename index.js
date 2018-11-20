ddee88fdc1e4cbf0271546fcf4edc56d5c8858ed
barneslardo <37343575+barneslardo@users.noreply.github.com>
2018-11-19 19:31:59 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var app = express();
+var ejs = require("ejs");
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
   res.send({hi: 'there'});
   console.log("main route hit");
+});
+
+app.get("/annie", function(req, res){
+  res.render("annie");
+  console.log("annie page hit");
+});
+
+app.get("*", function(req, res){
+  res.render("catch");
+  console.log("catch hit");
 });
 
 const PORT = process.env.PORT || 5000;
